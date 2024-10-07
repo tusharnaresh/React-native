c7e6d0fa18d11a6dd2488583cf9ce958654abf4d
tusharnaresh <tushar.naresh@anywhere.co>
2024-10-07 20:37:38 +0530

    added inject javascript to webview

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
-
+const INJECTED_JAVASCRIPT = `(function() {
+ const meta = document.createElement('meta'); 
+        meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'); 
+        meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); 
+})();`;
 export default function App() {
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://calendar.switchport.app/110' }} style={styles.webview} />
+      <WebView source={{ uri: 'https://calendar.switchport.app/110' }} style={styles.webview}
+      onMessage={(event) => {}}
+      injectedJavaScript={INJECTED_JAVASCRIPT} 
+      webviewDebuggingEnabled={true}
+      />
     </View>
   );
 }
